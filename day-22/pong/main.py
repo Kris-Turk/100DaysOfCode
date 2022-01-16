701c6c7e77af38757fafe5aade8dc3e9d1356743
Kris Turk <kris.turk@hybrit.co.nz>
2022-01-16 17:13:12 +1300

    completed pong game

--- day-22/pong/main.py
+++ day-22/pong/main.py
@@ -1,42 +1,70 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 import time
+from scoreboard import Scoreboard
 
 # Create the Screen
 screen = Screen()
-screen.setup(width=1200, height=600)
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0)
+screen.listen()
 
 
 # Create & Move the Paddle
 
-paddle = Paddle()
-
-
-screen.listen()
-screen.onkey(paddle.up,"Up")
-screen.onkey(paddle.down,"Down") 
-
+r_paddle = Paddle(350)
+screen.onkey(r_paddle.up,"Up")
+screen.onkey(r_paddle.down,"Down") 
 
 # Create another Paddle
 
-# Create the ball and move it
-
-# Detect collision with wall and bounce
-
-# Detect collision with Paddle
-
-# detect when paddle misses (ball passes end)
+l_paddle = Paddle(-350)
+screen.onkey(l_paddle.up,"w")
+screen.onkey(l_paddle.down,"s") 
 
 #  Scoreboard (keep score)
+
+scoreboard = Scoreboard()
+
+# Create the ball and move it
+
+ball = Ball()
 
 game_is_on = True
 
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
-    time.sleep(.1)
+    ball.move()
+    
+# Detect collision with wall and bounce
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()    
+
+# Detect collision Paddle
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320:
+        ball.bounce_x()
+        ball.increase_move_speed()
+        
+    if ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+        ball.increase_move_speed()
+        
+# detect when r_paddle miss
+
+    if ball.xcor() > 380:
+        ball.reset()
+        scoreboard.l_point()    
+        
+# detect when l_paddle miss
+    if ball.xcor() < -380:
+        ball.reset()  
+        scoreboard.r_point()  
 
 
 
--- day-22/pong/paddle.py
+++ day-22/pong/paddle.py
@@ -1,46 +1,22 @@
 from turtle import Turtle
-STARTING_POSITIONS = [(-560,20),(-560,0),(-560,-20)]
-MOVE_DISTANCE = 20
 
-class Paddle():
-    def __init__(self):
-        self.segments = []
-        self.create_paddle()
-        self.top = self.segments[0]
-        self.bottom = self.segments[-1]
-        
-    def create_paddle(self):
-        for pos in STARTING_POSITIONS:
-            self.add_segment(pos)
-    
-    def add_segment(self, pos):
-        new_segment = Turtle("square")
-        new_segment.color("white")
-        new_segment.penup()
-        new_segment.setposition(pos)
-        self.segments.append(new_segment)
-        
+class Paddle(Turtle):    
+    def __init__(self, start_y):
+            super().__init__()
+            self.shape("square")
+            self.color("white")
+            self.shapesize(stretch_wid=5, stretch_len=1)
+            self.penup()
+            self.goto(start_y,0)    
+   
+      
     def up(self):
-        self.segments[0].setheading(90)
-        for seg in range(len(self.segments)-1,0, -1):
-            new_x = self.segments[seg -1].xcor()
-            new_y = self.segments[seg -1].ycor()
-            self.segments[seg].goto(new_x,new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
-        
-    # def up(self):
-    #     self.segments[0].setheading(90)
-    #     self.move()
-    
-    # def down(self):
-    #     self.segments[-1].setheading(270)
-    #     self.move()
-        
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+
     def down(self):
-        self.segments[-1].setheading(270)
-        for seg in range(0, len(self.segments)-1, 1):
-            new_x = self.segments[seg +1].xcor()
-            new_y = self.segments[seg +1].ycor()
-            self.segments[seg].goto(new_x,new_y)
-        self.segments[-1].forward(MOVE_DISTANCE)
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)     
+
         
